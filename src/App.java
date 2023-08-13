aea29eaac961d62a4b1a0ab82281618bb538360c
qzhang16 <qzhang16@ra.rockwell.com>
2023-08-13 23:08:21 +0800

    leverage jndi.properties

--- src/App.java
+++ src/App.java
@@ -1,12 +1,9 @@
 
 
-import javax.jms.Connection;
-import javax.jms.ConnectionFactory;
-import javax.jms.MessageConsumer;
-import javax.jms.MessageProducer;
+
+import javax.jms.JMSContext;
 import javax.jms.Queue;
-import javax.jms.Session;
-import javax.jms.TextMessage;
+import javax.naming.InitialContext;
 
 import org.apache.activemq.artemis.jms.client.ActiveMQConnectionFactory;
 
@@ -14,37 +11,48 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
 
-        Connection connection = null;
-      try {
+        //     Connection connection = null;
+        //   try {
 
-         ConnectionFactory cf = new ActiveMQConnectionFactory();
+        //      ConnectionFactory cf = new ActiveMQConnectionFactory();
 
-         connection = cf.createConnection();
+        //      connection = cf.createConnection();
 
-         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        //      Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 
-         Queue queue = session.createQueue("exampleQueue");
+        //      Queue queue = session.createQueue("exampleQueue");
 
-         MessageProducer producer = session.createProducer(queue);
+        //      MessageProducer producer = session.createProducer(queue);
 
-         TextMessage message = session.createTextMessage("This is a text message");
+        //      TextMessage message = session.createTextMessage("This is a text message");
 
-         System.out.println("Sent message: " + message.getText());
+        //      System.out.println("Sent message: " + message.getText());
 
-         producer.send(message);
+        //      producer.send(message);
 
-         MessageConsumer messageConsumer = session.createConsumer(queue);
+        //      MessageConsumer messageConsumer = session.createConsumer(queue);
 
-         connection.start();
+        //      connection.start();
 
-         TextMessage messageReceived = (TextMessage) messageConsumer.receive(5000);
+        //      TextMessage messageReceived = (TextMessage) messageConsumer.receive(5000);
 
-         System.out.println("Received message: " + messageReceived.getText());
-      } finally {
-          if (connection != null) {
-              connection.close();
-          }
-       
-      }
+        //      System.out.println("Received message: " + messageReceived.getText());
+        //   } finally {
+        //       if (connection != null) {
+        //           connection.close();
+        //       }
+
+        //   }
+
+InitialContext context = new InitialContext();
+		Queue queue = (Queue) context.lookup("queue/queue01");
+		
+		try(ActiveMQConnectionFactory cf = new ActiveMQConnectionFactory();
+				JMSContext jmsContext = cf.createContext()){
+			jmsContext.createProducer().send(queue,"Arise Awake and stop not till the goal is reached");
+			String messageReceived = jmsContext.createConsumer(queue).receiveBody(String.class);
+			System.out.println(messageReceived);
+		}
+
     }
 }
